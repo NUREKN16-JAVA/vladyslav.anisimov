41e0e82e025747f899cb64bf98145b409c5d8d6f
vladyslav-anisimov <vladyslav.anisimov@nure.ua>
2018-11-23 11:28:47 +0200

    init project

---
+++ usermanagement/src/main/java/ua/nure/kn/anisimov/usermanagement/User.java
@@ -0,0 +1,55 @@
+package ua.nure.kn.anisimov.usermanagement;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import  java.time.temporal.ChronoUnit;
+
+public class User implements Serializable{
+
+	private static final long serialVersionUID = 4343198863391136205L;
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private Date dateOfBirth;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	
+	public String getFullName() {
+		return (new StringBuilder(getLastName()))
+				.append(", ")
+				.append(getFirstName())
+				.toString();
+	}
+
+    public long getAge(){
+        LocalDate currentDate = LocalDate.now();
+        LocalDate date = dateOfBirth.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        return ChronoUnit.YEARS.between(date, currentDate );
+    }
+	
+}
---
+++ usermanagement/src/test/java/ua/nure/kn/anisimov/usermanagement/UserTest.java
@@ -0,0 +1,74 @@
+package ua.nure.kn.anisimov.usermanagement;
+
+import java.util.Calendar;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class UserTest extends TestCase {
+	private static final String FULL_NAME_ETALONE = "Ivanov, Ivan";
+	private static final String LAST_NAME_ETALONE = "Ivanov";
+	private static final String FIRST_NAME_ETALONE = "Ivan";
+	private User user;
+
+
+	private static final int YEAR_OF_BIRTH = 1998;
+
+	private static final int ETALONE_AGE_1 = 20;
+	private static final int DAY_OF_BIRTH_1 = 15;
+	private static final int MONTH_OF_BIRTH_1 = Calendar.NOVEMBER;
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		user  = new User();
+	}
+
+	@Test
+	public void getFullName() {
+		user.setFirstName(FIRST_NAME_ETALONE);
+		user.setLastName(LAST_NAME_ETALONE);
+		assertEquals(FULL_NAME_ETALONE, user.getFullName());
+	}
+    @Test
+    public void testGetAge(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH_1, DAY_OF_BIRTH_1);
+        user.setDateOfBirth(calendar.getTime());
+        assertEquals(ETALONE_AGE_1, user.getAge());
+    }
+
+    @Test
+    public void testGetAgeWhenMonthAfterBDay(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR_OF_BIRTH, Calendar.getInstance().get(Calendar.MONTH)+1, DAY_OF_BIRTH_1);
+        user.setDateOfBirth(calendar.getTime());
+        assertEquals(ETALONE_AGE_1-1, user.getAge());
+    }
+    @Test
+    public void testGetAgeWhenMonthBeforeBDay()
+    {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR_OF_BIRTH, Calendar.getInstance().get(Calendar.MONTH)-1, DAY_OF_BIRTH_1);
+        user.setDateOfBirth(calendar.getTime());
+        assertEquals(ETALONE_AGE_1, user.getAge());
+    }
+    @Test
+    public void testGetAgeWhenDayAfterBDay()
+    {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR_OF_BIRTH, Calendar.getInstance().get(Calendar.MONTH) , Calendar.getInstance().get(Calendar.DAY_OF_MONTH)+1 );
+        user.setDateOfBirth(calendar.getTime());
+        assertEquals(ETALONE_AGE_1-1, user.getAge());
+    }
+    @Test
+    public void testGetAgeWhenDayBeforeBday()
+    {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR_OF_BIRTH, Calendar.getInstance().get(Calendar.MONTH) , Calendar.getInstance().get(Calendar.DAY_OF_MONTH)-1);
+        user.setDateOfBirth(calendar.getTime());
+        assertEquals(ETALONE_AGE_1, user.getAge());
+    }
+}
